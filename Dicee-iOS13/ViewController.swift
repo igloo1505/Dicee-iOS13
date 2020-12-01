f260b2faeeca19e0934541d87b395e8ebf2281c8
Andrew <iglooworks1505@gmail.com>
2020-12-01 02:52:46 -0600

    just adding this for the comments for future reference. My VERY beggining into swift

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageOne: UIImageView!
+    @IBOutlet weak var diceImageTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        use 'image literal' to select image, instead of getElementByID
+        diceImageOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageTwo.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+//        To use string literals, use \(variableHere) instead of ${thisInJavascript}
+        print("Button was pressed")
+//        Let in swift is a constant. Use var for variables, and let for constants. const is not a key term
+        let imageArray = [
+            #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")
+        ]
+        let randomIndexOne = (Int(floor(Float.random(in: 0...1) * Float(imageArray.count))))
+        let randomIndexTwo = (Int(floor(Float.random(in: 0...1) * Float(imageArray.count))))
 
+        diceImageOne.image = imageArray[randomIndexOne]
+//        NOTE arrays in swift have randomElement method, that does the same as Math.floor(Math.random...) in Javascript!
+        diceImageTwo.image = imageArray.randomElement()
+    }
+    
 }
 
